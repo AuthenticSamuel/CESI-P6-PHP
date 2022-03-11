16b74e4f3556c586781d951f62a112da606c6509
INF-Zenyth <samuel.ward@outlook.fr>
2022-03-11 09:11:05 +0100

    p6

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/styles.css">
+	<title>P6 - PHP One</title>
+</head>
+
 <?php
-	phpinfo();
+$data = file_get_contents("./assets/emoji.json");
+$jsonData = json_decode($data, true);
+
+// var_dump($jsonData);
+?>
+
+<body>
+	<div id="emoji-display-container">
+		<div id="emoji-display">
+			<?php
+			$index = 0;
+			$page = 2;
+			foreach ($jsonData as $key => $value) {
+				if ($index >= 256 * $page) return;
+				$emoji = $value["code_decimal"];
+
+				echo "<p class='emoji'>$emoji</p>";
+
+				$index++;
+			}
+			?>
+		</div>
+	</div>
+
+</body>
+
+</html>
